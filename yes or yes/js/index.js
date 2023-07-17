baf1aafaac54e8e69a8b8e2c719e5a120e09ea69
creamywhite <51010201+saireniva@users.noreply.github.com>
2023-07-17 16:08:29 +0800

    Add files via upload

---
+++ yes or yes/js/index.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//   // $('#content').pushpin({
+//   //   top: $('#content').offset().top
+//   // });
+//   $('.scrollspy').scrollSpy({
+//     scrollOffset: 0
+//   });
+// });
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
+
